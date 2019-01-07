0859ab8938a9b50b7851bf59fb754c7a4dab3f34
x <x@y>
2019-01-08 01:11:00 +0800

    a

--- daily/cobra.go
+++ daily/cobra.go
@@ -23,18 +23,19 @@ var clientCmd = &cobra.Command{
 	},
 }
 
-var server string
 var listen string
+var server string
 
 func init() {
+
 	cobra.OnInitialize(initConfig)
 
 	rootCmd.AddCommand(serverCmd, clientCmd)
 
-	serverCmd.PersistentFlags().StringVarP(&listen, "listen", "l", ":12345", "listen address")
+	serverCmd.PersistentFlags().StringVarP(&listen, "listen", "l", "127.0.0.1:12345", "listen address")
 	serverCmd.MarkPersistentFlagRequired("listen")
 
-	clientCmd.Flags().StringVarP(&server, "server", "s", ":12345", "server address")
+	clientCmd.Flags().StringVarP(&server, "server", "s", "127.0.0.1:12345", "server address")
 	clientCmd.MarkFlagRequired("server")
 
 }
@@ -44,11 +45,13 @@ func initConfig() {
 }
 
 func Execute() {
+
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func main() {
+
 	Execute()
 }
